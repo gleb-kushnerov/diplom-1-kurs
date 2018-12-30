c9395f7bac76745530aa870ad9041bdbe50f6ea0
Che1sea57 <38821256+Che1sea57@users.noreply.github.com>
2018-12-30 16:32:47 +0300

    сверстал макет хедера, добавил миксины, переменные

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./src/style/**/*.scss', ['sass']);
+    gulp.watch('./src/style/**/*.scss', gulp.series('sass'));
 });
